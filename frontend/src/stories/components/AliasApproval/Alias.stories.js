6fe374915ae052d719d52da8b7f5c762983d8c9b
Renbo <renbo@ucsb.edu>
2025-05-22 11:14:31 -0700

    rb-add code

---
+++ frontend/src/stories/components/AliasApproval/Alias.stories.js
@@ -0,0 +1,32 @@
+// import React from "react";
+// import { action } from "@storybook/addon-actions";
+// import AliasTable from "main/components/Alias/AliasTable";
+// import aliasFixtures from "fixtures/aliasFixtures";
+
+// export default {
+//   title: "Components/AliasApproval/AliaslTable",
+//   component: AliasTable,
+// };
+
+// const Template = (args) => <AliasTable {...args} />;
+
+// export const Default = Template.bind({});
+// Default.args = {
+//   commons: aliasFixtures.threeAlias,
+//   onApprove: action("onApprove"),
+//   onReject: action("onReject"),
+// };
+
+// export const NoAliases = Template.bind({});
+// NoAliases.args = {
+//   commons: [],
+//   onApprove: action("onApprove"),
+//   onReject: action("onReject"),
+// };
+
+// export const SingleAlias = Template.bind({});
+// SingleAlias.args = {
+//   commons: [aliasFixtures.oneAlias],
+//   onApprove: action("onApprove"),
+//   onReject: action("onReject"),
+// };
